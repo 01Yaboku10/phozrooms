0c26899733c51cbbc775e2ce66cac6ccaad3af07
Yabo <122304460+01Yaboku10@users.noreply.github.com>
2024-11-01 14:20:23 +0100

    Updated main.py
    Added annotation and docstring and minor changes

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+"""Huvudmodul för huvudprogrammet av Phöz Rooms"""
+
 import menu
 import movement
 import generation
 import failsafe
 
-def main():
+def main() -> None:
+    """Huvudprogrammet för Phöz Rooms"""
     rto = True
     while rto:
         main_menu = menu.Main_menu()
-        game_map, mapsize = main_menu.menu()  #  kör menyn och retunerar spelbanan och dess storlek
+        game_map, mapsize, difficulty = main_menu.menu()  #  kör menyn och retunerar spelbanan och dess storlek
         movement.Start(game_map)
         admin = failsafe.set_admin
-        movement_inst = movement.Movement(game_map, mapsize, admin)
-        generation.Generation(game_map, mapsize, movement_inst)
-        movement_inst.start_move()
+        movement_inst = movement.Movement(game_map, mapsize, admin, difficulty)
+        generation.Generation(game_map, mapsize)
+        movement_inst.start_move(admin)
         rto = failsafe.rto_menu()
     print("Quitting Phöz Rooms...")
 
-main()
+if __name__ == "__main__":
+    main()
