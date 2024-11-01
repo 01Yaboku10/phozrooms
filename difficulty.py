1ff072ab6cdb41714c686d680f50286de7ccf0a0
Yabo <122304460+01Yaboku10@users.noreply.github.com>
2024-11-01 14:18:44 +0100

    Updated difficulty.py
    added annotation and docstring

--- difficulty.py
+++ difficulty.py
@@ -1,15 +1,29 @@
+"""Modul för Phöz Rooms som sätter värden på de olika svårighetsgraderna"""
+
 class Difficulty():
-    def __init__(self, MAPSIZE, PHOZ, HOLE, TREASURE, BATS):
-        self.mapsize = 5 + MAPSIZE
+    """Klass för att sätta värden på svårighetsgrader"""
+    def __init__(self, mapsize: int, phoz: int, hole: int, treasure: int, bats: int) -> None:
+        """Sätter värdena för de olika svårighetsgraderna
+                Argument:
+                    mapsize(int): kartstorlek
+                    phoz(int): mängd phöz
+                    hole(int): mängd hål
+                    treasure(int): mängd skatt
+                    bats(int): mängd fladdermöss
+                Retunerar:
+                    None"""
+        self.mapsize = 5 + mapsize
         self.START = 1
-        self.phoz = 1 + PHOZ
-        self.hole = 20 + HOLE
-        self.treasure = 1 + TREASURE
-        self.bats = 30 + BATS
-    def diff_attrib(self):
+        self.phoz = 1 + phoz
+        self.hole = 20 + hole
+        self.treasure = 1 + treasure
+        self.bats = 30 + bats
+
+    def diff_attrib(self) -> tuple[int, int, int, int, int]:
+        """Retunerar svårighetsgrads värderna"""
         return self.mapsize, self.phoz, self.hole, self.treasure, self.bats
 
 easy = Difficulty(0, 0, 0, 0, 0)
-normal = Difficulty(3, 2, 5, 2, 5)
-hard = Difficulty(5, 3, 10, 2, 10)
-fadder = Difficulty(11, 4, 10, 3, 10)
+normal = Difficulty(3, 1, 5, 2, 5)
+hard = Difficulty(5, 2, 10, 2, 10)
+fadder = Difficulty(11, 3, 10, 3, 10)
