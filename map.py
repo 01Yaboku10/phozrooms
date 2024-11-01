2c7963e6d54e674305e8f1e5f4033d0dd59aa34c
Yabo <122304460+01Yaboku10@users.noreply.github.com>
2024-11-01 14:21:04 +0100

    Updated map.py
    Added annotation, docstrings and minor changes

--- map.py
+++ map.py
@@ -1,30 +1,32 @@
+"""Modul för att skapa spelkartan till Phöz Rooms"""
+
 import failsafe
 import generation
-import movement
 
-MAPSIZE = 5
 START = 1
-PHOZ = 1
-HOLE = 20  #  I %
-TREASURE = 1
-BATS = 30  #  I %
 
 class Map():
+    """Klass för kartan"""
 
-    def __init__(self, diff, admin):
+    def __init__(self, diff: object, admin: bool) -> None:
+        """initialiserande metod som först genererar en tom spelkarta och sedan
+        kallar på en metod för att fylla den med objekt.
+            Argument:
+                diff(object): objekt för svårighetsgrads klassen.
+                admin(bool): spelarens admin status
+            Retunerar:
+                None"""
         self.mapsize, self.phoz, self.hole, self.treasure, self.bats = diff.diff_attrib()
 
         self.map = [[0 for _ in range(self.mapsize)] for _ in range(self.mapsize)]  #  skapar en mapsize x mapsize stor lista, där _ är en temporär variabel
         generation.spawner(self.map, self.mapsize, START, self.phoz, self.hole, self.treasure, self.bats, admin)
-    def __str__(self):
-        return str(self.map)
 
-    def generate_map(self, admin):
-        """Genererar spelkartan. Kollar först om mängden kartobjekt
+    def generate_map(self, admin: bool) -> None:
+        """Skriver ut spelkartan. Kollar först om mängden kartobjekt
         överstiger antalet 'rum' i matrisen, och genererar isåfall ingen karta."""
         if failsafe.is_spawn_limit(self.mapsize, START, self.phoz, self.hole, self.treasure, self.bats):
-            if admin:   
+            if admin:  
                 for row in self.map:
                     print(row)
         else:
-            print("Spawn items exceeds that of available spawn points")
+            print("ERROR: Spawn items exceeds that of available spawn points.")
